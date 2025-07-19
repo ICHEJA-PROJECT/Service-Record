84b766a3ffcfbe97f1d52251977a610c534ff7c8
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-07-19 16:12:19 -0600

    feat: update pupil controllers to use MessagePattern for microservices communication

--- src/pupilTopic/controllers/pupil_topic.controller.ts
+++ src/pupilTopic/controllers/pupil_topic.controller.ts
@@ -1,20 +1,20 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from "@nestjs/common";
-import { CreatePupilTopicDto } from "../data/dtos/create-pupil-topic.dto";
-import { PupilTopicService } from "../services/pupil_topic.service";
+import { Controller } from '@nestjs/common';
+import { CreatePupilTopicDto } from '../data/dtos/create-pupil-topic.dto';
+import { PupilTopicService } from '../services/pupil_topic.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { RECORD_SERVICE_OPTIONS } from 'src/shared/constants/record_service_options';
 
 @Controller('pupil-topics')
 export class PupilTopicController {
-    constructor(private readonly pupilTopicService: PupilTopicService) {}
+  constructor(private readonly pupilTopicService: PupilTopicService) {}
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    async create(@Body() createPupilTopicDto: CreatePupilTopicDto) {
-        return await this.pupilTopicService.create(createPupilTopicDto);
-    }
+  @MessagePattern({ cmd: RECORD_SERVICE_OPTIONS.PUPIL_TOPIC_CREATE })
+  async create(@Payload() createPupilTopicDto: CreatePupilTopicDto) {
+    return await this.pupilTopicService.create(createPupilTopicDto);
+  }
 
-    @Get(':id')
-    @HttpCode(HttpStatus.OK)
-    async getByPupil(@Param('id') id: number) {
-        return await this.pupilTopicService.findByPupil(id);
-    }
+  @MessagePattern({ cmd: RECORD_SERVICE_OPTIONS.PUPIL_TOPIC_FIND_BY_PUPIL_ID })
+  async getByPupil(@Payload() id: number) {
+    return await this.pupilTopicService.findByPupil(id);
+  }
 }
